734095bc0d339b7d7734b38b8e02574d016f2f23
Amarthya <amarthyaseshu2@gmail.com>
2020-06-06 15:14:32 +0530

    FSJ0320B006

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
@@ -25,16 +27,21 @@ public class SignUpController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		System.out.println("Sign up Controller");
-//		Connection con=null;
-//		con=ConnectionManager.getConnection();
-//		if(con!=null) {
-//			System.out.println("Database connection established");
-//		}
-//		else
-//		{
-//			System.out.println("Check your connection");
-//		}
+		System.out.println("Sign up Controller");
+		Connection con=null;
+		try {
+			con=ConnectionManager.getConnection();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(con!=null) {
+			System.out.println("Database connection established");
+		}
+		else
+		{
+			System.out.println("Check your connection");
+		}
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
 		rd.forward(request,response);
 	}
@@ -51,10 +58,16 @@ public class SignUpController extends HttpServlet {
 		
 		User user=new User();
 		user.setEmail(email);
-		user.setPassword(password);
+		user.setPassword(confirmPassword);
 		UserDAO userdao=new UserDAO();
 		user.setDate(date);
-		int checkUser = userdao.signUp(user);
+		int checkUser = 0;
+		try {
+			checkUser = userdao.signUp(user);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		if(checkUser!=0)
 		{
--- src/dao/BlogDaoInterface.java
+++ src/dao/BlogDaoInterface.java
@@ -1,2 +1,12 @@
 package dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Blog;
+
+interface BlogDaoInterface{
+	
+	void insertBlog(Blog blog) throws ClassNotFoundException, SQLException;
+	List<Blog> selectAllBlogs() throws ClassNotFoundException, SQLException;
+}
